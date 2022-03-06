a43c3321c8f33bfbb59df9794b5c1b497a323b51
The Halfrican <noahmattocks@gmail.com>
2022-03-05 18:45:18 -0800

    Added Quiz functionality in JS

--- script.js
+++ script.js
@@ -1,32 +1,112 @@
-const startButton = document.getElementById("start-btn")
-const qeustionContainerEl = document.getElementById("qeustion-container")
-const shuffledQeustions, currentQeustionIndex
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
-
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 function startGame() {
-console.log('started')
-startButton.classList.add('hide')
-shuffledQeustions = qeustions.sort(() => Math.random)
-qeustionContainerEl.classList.remove("hide")
-setNextQeustion()
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-function setNextQeustion() { 
-
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
-
-}
-
-const qeustions = [
-    {
-        qeustion: "What is the symbol to call an ID?",
-        answer: [
-            { text: "#", correct: true },
-            { text: "%", correct: false},
-        ] 
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
     }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is the symbol to call an ID?',
+    answers: [
+      { text: '#', correct: true },
+      { text: '%', correct: false }
+    ]
+  },
+  {
+    question: 'What does CSS stand for?',
+    answers: [
+      { text: 'Copy Spider Spunk', correct: false },
+      { text: 'Cascading Style Sheets', correct: true },
+      { text: 'Combination Set Styles', correct: false },
+      { text: 'Company Serrated Status', correct: false }
+    ]
+  },
+  {
+    question: 'What is a const?',
+    answers: [
+      { text: 'An array that never goes away', correct: false },
+      { text: 'A constant pain in the butt!', correct: false },
+      { text: 'An object called by a function that cannot be modified', correct: true },
+      { text: 'A true OG to the end, a real ride or die homie.', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
 ]
